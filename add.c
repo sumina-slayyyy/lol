83a5a039e17d519cf53c925956addf5eac1cfadb
Sumi <suminabista365@gmail.com>
2026-05-05 15:20:06 +0545

    Add files via upload

---
+++ add.c
@@ -0,0 +1,34 @@
+/*#include<stdio.h>
+void main()
+{
+    int a = 3;
+    int b = 5;
+    int sum;
+    sum = a + b;
+    printf("%d",sum);
+    
+getch();
+}*/
+/*#include<stdio.h>
+void main()
+{
+    int a = 3;
+    int b = 5;
+    int sum;
+    sum = a - b;
+    printf("%d",sum);
+    getch();
+
+}*/
+#include<stdio.h>
+void main()
+{
+    int a = 3;
+    int b = 5;
+    int multiply;
+    multiply = a * b;
+    printf("multiply of %d and %d is %d",a,b,multiply);
+    getch();
+
+
+}
