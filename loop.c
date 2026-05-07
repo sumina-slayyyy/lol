cdd92bd1d4e15e308fa343c438ddfc5504ea9fc9
Sumi <suminabista365@gmail.com>
2026-05-07 21:49:18 +0545

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,33 @@
+//for loop
+/*#include <stdio.h>
+int main(){
+    int i;
+    for(i=0;i<10;i++){
+        printf("value of i: %d\n",i);
+    }
+
+}*/
+
+
+
+//while loop
+/*#include<stdio.h>
+int main(){
+    int i=0;
+    while(i<10){
+        printf("value of i: %d\n",i);
+        i++;
+    }
+}*/
+
+
+//do while loop
+#include<stdio.h>
+int main(){
+    int i=0;
+    do{
+        printf("value of i: %d\n",i);
+        i++;
+    }
+    while(i<10); 
+    }
